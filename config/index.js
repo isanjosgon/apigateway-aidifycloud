ddca5491f4d7409b7bcdf2b3c1a60253cf173063
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-19 20:28:18 +0100

    Update config file

--- config/index.js
+++ config/index.js
@@ -3,6 +3,42 @@
 
 'use strict'
 
+const config = require('../package.json');
 const env = require('node-env-file');
 let file = process.env.NODE_ENV || 'dev';
 env(__dirname + '/.' + file);
+
+module.exports = {
+  environment : process.env.NODE_ENV || 'dev',
+  restapi : {
+    port : process.env.PORT || 5001,
+    name : config.name,
+    version : config.version
+  },
+  messagebroker : {
+    host : process.env.REDIS_HOST,
+    port : process.env.REDIS_PORT,
+    pass : process.env.REDIS_PASSWORD
+  },
+  logging : {
+    appenders: [{
+      type: 'file',
+      filename: 'logs/error.log',
+      category: 'error',
+      maxLogSize: 20480,
+      backups: 10
+    },{
+      type: 'file',
+      filename: 'logs/info.log',
+      category: 'info',
+      maxLogSize: 20480,
+      backups: 10
+    },{
+      type: 'file',
+      filename: 'logs/debug.log',
+      category: 'debug',
+      maxLogSize: 20480,
+      backups: 10
+    }]
+  }
+}
