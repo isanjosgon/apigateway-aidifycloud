5d73d4122c0acc33875da846cbef23a823283049
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-21 09:43:43 +0100

    Suscribe to APIGATEWAY channel

--- messagebroker/broker.js
+++ messagebroker/broker.js
@@ -7,13 +7,20 @@ const redis = require('redis');
 
 class Broker
 {
-  constructor (config) {
+  constructor (config,logger) {
     if (config.host) {
       this.client = redis.createClient(config.host,config.port);
       this.client.auth(config.pass);
     } else {
       this.client = redis.createClient();
     }
+    this.client.subscribe('SERVICE:APIGATEWAY');
+    this.client.on('message',function (channel,message) {
+      let service = channel.split(':')[1];
+      if (service == 'APIGATEWAY') {
+        console.log(message);
+      }
+    });
   }
 }
 
