c991517ca0bde7007430342f8c59669fefef9bf2
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-22 23:52:09 +0100

    Connect everything

--- app.js
+++ app.js
@@ -9,11 +9,26 @@ const Logger = require('./interface/logger');
 const Server = require('./restapi/server');
 const Broker = require('./messagebroker/broker');
 
+const UserMapper = require('./model/user/usermapper');
+const CacheStrategy = require('./model/user/cachestrategy');
+const DataStrategy = require('./model/user/datastrategy');
+const UserRepository = require('./model/user/userrepository');
+
 const GetUserUseCase = require('./usecase/getuser');
+const CreateUserUseCase = require('./usecase/createuser');
+const LocateUserUseCase = require('./usecase/locateuser');
+const InvalidateUserUseCase = require('./usecase/invalidateuser');
 
 let logger = new Logger(config.logging);
 
-let getUserUseCase = new GetUserUseCase();
+let cacheStrategy = new CacheStrategy(config);
+let dataStrategy = new DataStrategy(config,UserMapper);
+let userrepo = new UserRepository(dataStrategy,cacheStrategy);
 
-new Server(config.restapi,logger);
-new Broker(config.messagebroker,logger);
+let getUserUseCase = new GetUserUseCase(userrepo);
+let createUserUseCase = new CreateUserUseCase(userrepo);
+let locateUserUseCase = new LocateUserUseCase(userrepo);
+let invalidateUserUseCase = new InvalidateUserUseCase(userrepo);
+
+new Server(config.restapi,logger,getUserUseCase,createUserUseCase,locateUserUseCase);
+new Broker(config.messagebroker,logger,invalidateUserUseCase);
