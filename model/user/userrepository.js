a75cd1aa8e72c3ac18ee6b87c5598f5e73ca0529
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-22 23:50:53 +0100

    Divide UserRepository into two strategies

--- model/user/userrepository.js
+++ model/user/userrepository.js
@@ -3,25 +3,64 @@
 
 'use strict'
 
-const request = require('superagent');
-
 class UserRepository
 {
-  constructor (config,parse) {
-    this.config = config;
-    this.parser = parser;
+  constructor (dataStrategy,cacheStrategy) {
+    this.dataStrategy = dataStrategy;
+    this.cacheStrategy = cacheStrategy;
+  }
+  create (token) {
+    const self = this;
+    return new Promise(function (resolve,reject) {
+      self.dataStrategy.create(token,function (err,user) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(user);
+        self.cacheStrategy.insert(user);
+      });
+    });
   }
   findbyId (id) {
-    var self = this;
+    const self = this;
     return new Promise(function (resolve,reject) {
-      request
-        .get(self.config.cloud.userservice + '/user/' + id)
-        .end(function (err,res) {
+      self.cacheStrategy.fetch(id,function (err,user) {
+        if (err) {
+          return reject(err);
+        }
+        if (user) {
+          return resolve(user);
+        }
+        self.dataStrategy.getbyId(id,function (err,user) {
           if (err) {
             return reject(err);
           }
-          resolve(self.parser.userfromjson(res.body));
+          resolve(user);
+          self.cacheStrategy.insert(user);
         });
+      });
+    });
+  }
+  locate (id,status) {
+    const self = this;
+    return new Promise(function (resolve,reject) {
+      self.dataStrategy.locate(function (err,user) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(user);
+      });
+    });
+  }
+  invalidate (id) {
+    const self = this;
+    return new Promise(function (resolve,reject) {
+      self.cacheStrategy.invalidate(id,function (err) {
+        if (err) {
+          return;
+        }
+        self.findbyId(id);
+      });
     });
   }
 }
